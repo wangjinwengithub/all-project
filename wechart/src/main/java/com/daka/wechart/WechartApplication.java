9781d50be889a75265f7eb9bc601e8decc520281
WangJW <1510712025@qq.com>
2019-06-12 18:30:33 +0800

    父子类maven打包插件的依赖调整

---
+++ wechart/src/main/java/com/daka/wechart/WechartApplication.java
@@ -0,0 +1,13 @@
+package com.daka.wechart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WechartApplication.class, args);
+    }
+
+}
